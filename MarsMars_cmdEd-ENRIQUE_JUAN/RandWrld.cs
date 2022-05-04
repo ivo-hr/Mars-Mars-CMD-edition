e84cfafc7d5996d30cb1603a644154b9f2d630d8
ivo_hr <81875802+ivo-hr@users.noreply.github.com>
2022-05-05 00:07:03 +0200

    lol

---
+++ MarsMars_cmdEd-ENRIQUE_JUAN/RandWrld.cs
@@ -0,0 +1,102 @@
+﻿//ENRIQUE JUAN GAMBOA
+//Proyecto final FPII
+
+using System;
+
+//GENERADOR DE MUNDOS ALEATORIOS
+namespace RandWrld_generator
+{
+    class RandWrld
+    {
+        //Ancho y altitud máxima del mundo
+        private int ancho, maxAlt;
+
+        //El mundo
+        private int[] wrld;
+        //Randomizador del terreno
+        Random rnd = new Random();
+
+        //Inicialización del mundo: ancho, alto y semilla para generar
+        public RandWrld(int ANCHO, int ALTOMAX, int oldWrld)
+        {
+            ancho = ANCHO;
+
+            maxAlt = ALTOMAX;
+
+            wrld = new int[ancho];
+
+            rnd = new Random(oldWrld);
+        }
+
+        //Devolver el mundo (readonly)
+        public int[] GetWorld()
+        {
+            return wrld;
+        }
+
+        //Generación del mundo 1 a 1
+         public void WorldGen(int features, int roughness)
+        {
+            for (int i = 0; i < ancho - 1; i++)
+            { // desplazamiento de eltos a la izda
+                wrld[i] = wrld[i + 1];
+            }
+
+            int s; // ultima posicion de suelo
+            s = wrld[ancho - 1];
+
+            // generamos nuevos valores para esa posicion
+            int opt = rnd.Next(features); //si varia
+            int var = rnd.Next(1, roughness); //Cuánto varia
+
+            //Límites de la variación
+            if (opt == 0 && s < maxAlt)
+                s +=var;
+            else if (opt == 1 && s > 3)
+                s -= var;
+
+
+            // asignamos ultima posicion en el array
+            wrld[ancho - 1] = s;
+        }
+
+        //Inicialización  del mundo
+        public void StartWorld(int feat, int roug )
+        {
+            //Se esconde el cursor para rederizado limpio
+            Console.CursorVisible = false;
+
+            //Primeros valores del techo
+            wrld[ancho - 1] = maxAlt - 1;
+
+            //Generación del mundo en la primera pantalla
+            for (int i = 0; i < ancho - 1; i++) WorldGen(feat, roug);
+        }
+
+        //Renderizado del mundo
+        public void RenderWorld(int floor, int renderSize)
+        {
+            //Cada columna
+            for (int i = 0; i < ancho; i++)
+            {
+                //La fila de cada columna
+                for (int j = 0; j < floor; j++)
+                {
+                    //Colocación del cursor
+                    Console.SetCursorPosition(renderSize * i, j);
+
+                    //Si es el suelo
+                    if (j >= wrld[i])
+                    {
+                        Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    }
+                    //Si no es nada
+                    else Console.BackgroundColor = ConsoleColor.Black;
+
+                    //Escritura de la posición
+                    for (int k = 0; k < renderSize; k++) Console.Write(" ");
+                }
+            }
+        }
+    }
+}
---
+++ Test/PhySick.cs
@@ -0,0 +1,124 @@
+﻿//ENRIQUE JUAN GAMBOA
+//Proyecto final FPII
+
+
+//Physics motor for "realistic simulation" of the astronaut
+//This method will be explained in English to ease explanations
+namespace PhySick_engine
+{
+    public class PhySick 
+    {
+        //Planet specs: gravity and resistance
+        private float grav = 10, res = 1, 
+        //Time units in planet, time since takeoff and speed limits
+            tick = 0, timSinceJump = 0, dangerDropSp = 10, maxf = 13;
+
+
+
+        //Simulated object: mass, downward  applied  force,  horizontal speed, latitude and longitude
+        struct MassObj
+        {
+            public float mass,
+                        latF,
+                        lonF,
+                        lat,
+                        lon;
+
+        }
+        MassObj obj = new MassObj();
+
+        //Planet and object constructor
+        public PhySick(float gravity, float airResist, float mass, float deathLim, float spLim, float refresh)
+        {
+            grav = gravity;
+            res = airResist;
+            obj.mass = mass;
+            dangerDropSp = deathLim;
+            maxf = spLim;
+            tick = refresh;
+
+        }
+
+        //Object position (lat and lon) get/set
+        public float[] objPos
+        {
+            get
+            {
+                return new float[2] { this.obj.lat, this.obj.lon };
+            }
+            set
+            {
+                this.obj.lat = value[0];
+                this.obj.lon = value[1];
+            }
+        }
+
+        //Object's  current forces
+        public float GetForce(int lat0lon1)
+        {
+            if (lat0lon1 > 0)
+            {
+                return obj.lonF;
+            }
+            else return obj.latF;
+        }
+
+        //World physics application
+        public void MainPhysics()
+        {
+            //Gravity
+            if (timSinceJump > 0)  obj.latF += grav * timSinceJump;
+
+            //Air resistance
+            if (obj.lonF < 0)
+                obj.lonF += res * timSinceJump ;
+            else if (obj.lonF > 0)
+                obj.lonF -= res * timSinceJump ;
+
+            //Object movement
+            if (timSinceJump > 0)
+            {
+                obj.lat += obj.latF * timSinceJump;
+                obj.lon += obj.lonF * timSinceJump;
+            }
+
+            
+
+           
+        }
+
+        //Force application upon the object: x = horizontal force applied, y = vertical force applied
+        public void ApplyForce(float x, float y)
+        {
+            obj.latF += y * timSinceJump;
+            
+            obj.lonF += x * timSinceJump;
+
+
+            if (obj.latF < -maxf) obj.latF = -maxf;
+        }
+
+        //Force cancelation upon the object
+        public void CancelForces()
+        {
+            obj.latF = 0;
+            obj.lonF = 0;
+        }
+        
+        //Time elapsed since last jump. Used to increase/decrease forces
+        public void TimeSinceFloor(bool count)
+        {
+            if (count) timSinceJump = (timSinceJump + tick) / 1000;
+            else
+            {
+                timSinceJump = 0;
+            }
+        }
+
+        //Dangerous entry detection
+        public bool FastEntry()
+        {
+            return obj.latF > dangerDropSp;
+        }
+    }
+}
